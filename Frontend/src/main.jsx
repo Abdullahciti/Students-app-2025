7ce1fbe0503a756fc9ad3ad91ad883d5f1604227
Abdullah <136592513+Abdullahciti@users.noreply.github.com>
2024-12-25 19:50:07 +0100

    Updated again from mobile main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -18,7 +18,7 @@ import ErrorPage from "./routes/ErrorPage.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AddStudent from "./routes/AddStudent.jsx";
 
-const baseUrl = `https://students-app-fullstack-backend.onrender.com/api/students`;
+const baseUrl = `https://students-app-fullstack-backend.onrender.com/api`;
 
 const router = createBrowserRouter([
   {
