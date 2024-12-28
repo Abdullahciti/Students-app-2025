ff1732770452e201602b5755e40822d28cbe9422
Abdullah Arafa <abdullahciti@gmx.de>
2024-12-28 10:55:35 +0100

    fixed the NavbarSm in error page

--- Frontend/src/routes/ErrorPage.jsx
+++ Frontend/src/routes/ErrorPage.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import NavbarLg from "../NavbarLg";
-import Navbarsm from "../Navbarsm";
+import NavbarSm from "./NavbarSm";
 
 function ErrorPage() {
   return (
     <div className="bg-mainColor w-screen h-screen flex">
       <NavbarLg />
-      <Navbarsm />
+      <NavbarSm />
       <div className="h-screen bg-transparent p-4 flex flex-col items-start lg:w-11/12 w-7/12 bg-black">
         <h1 className="text-4xl w-fit mx-auto mt-16 text-black font-bold">
           Sorry, an unexpected error has occurred or you have missed the right
